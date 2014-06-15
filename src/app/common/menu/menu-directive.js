3f5e99e83c097147bd6b9402fb224c156e809ae9
Jogga <joachim@froestl.com>
2014-06-15 23:51:24 +0200

    active tab highlighting

--- src/app/common/menu/menu-directive.js
+++ src/app/common/menu/menu-directive.js
@@ -4,13 +4,24 @@
     .directive('mainMenu', mainMenu);
 
 
-    function mainMenu() {
+    function mainMenu($location) {
         // body...
         return {
             replace: true,
             restrict: 'E',
-            templateUrl: 'common/menu/main-menu.tpl.html'
+            templateUrl: 'common/menu/main-menu.tpl.html',
+            link: linkFn
         };
+
+        function linkFn(scope, el, attrs) {
+            scope.isActive = function (path) {
+                if ($location.path().substring(1).indexOf(path.substring(1)) !== -1 && path.substring(1) !== '' ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+        }
 
     }
 
