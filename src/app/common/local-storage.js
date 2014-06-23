18e000f5479c42098b0139d160464b2ccdd44ed9
Sebastian Fröstl <sebastian@froestl.com>
2014-06-23 22:57:16 +0200

    feat(localStorage): add service

---
+++ src/app/common/local-storage.js
@@ -0,0 +1,33 @@
+(function (sandbox) {
+    'use strict';
+
+    sandbox.angular.module('amb.localStorage', [])
+    .factory('localStorage', function () {
+        var tokenKey = 'ato';
+
+        var localStorage = sandbox.localStorage || {
+            setItem: function () {},
+            getItem: function () {},
+            removeItem: function () {}
+        };
+
+        function setAccessToken(token) {
+            localStorage.setItem(tokenKey, token);
+        }
+
+        function getAccessToken() {            
+            return localStorage.getItem(tokenKey);
+        }
+
+        function deleteAccessToken() {
+            localStorage.removeItem(tokenKey);
+        }
+
+        return {
+            setAccessToken: setAccessToken,
+            getAccessToken: getAccessToken,
+            deleteAccessToken: deleteAccessToken
+        };
+    });
+
+}(this));
