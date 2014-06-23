af00cf5910a9c983416f259dd5687f047a2c2952
Sebastian Fröstl <sebastian@froestl.com>
2014-06-23 23:05:36 +0200

    feat(register): add register flow

--- src/app/screens/login/login-controller.js
+++ src/app/screens/login/login-controller.js
@@ -2,17 +2,24 @@
     'use strict';
 
     exports.angular.module('amb.login.loginController', [
-        'amb.model.User'
+        'amb.model.User',
+        'amb.common.AuthService'
     ]).controller('loginController', loginController);
 
-    function loginController($scope, User) {
-        $scope.login = function (loginData) {
-            User.login(loginData).$promise.then(retrieveAndSaveToken);
+    function loginController($scope, $location, User, AuthService) {
+
+        $scope.login = function (user) {
+            AuthService.login(user.email, user.password).then(function () {
+                $location.path('/activities');
+            });
         };
 
-        function retrieveAndSaveToken(token) {
-            console.log(token);
-        }
+        $scope.register = function (user) {
+            User.save({email: user.email, password: user.password}).$promise.then(function (newUser) {
+                newUser.password = user.password;
+                $scope.login(newUser);
+            });
+        };
     }
 
 }(this));
