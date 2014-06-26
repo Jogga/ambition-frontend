f36f477969e4d64fd3664712031e24fe91ea0792
Sebastian Fröstl <sebastian@froestl.com>
2014-06-26 15:13:54 +0200

    feat(inlineEdit) add directive and template

---
+++ src/app/ui/inline-edit/inline-edit-directive.js
@@ -0,0 +1,64 @@
+(function (exports) {
+    'use strict';
+
+
+    exports.angular.module('amb.ui.inlineEdit', [])
+    .directive('inlineEdit', InlineEdit);
+
+
+    function InlineEdit() {
+        return {
+            restrict: 'A',
+            scope: {
+                model: '=inlineEdit',                
+                doSave: '&onSave',
+                doCancel: '&onCancel'
+            },
+            templateUrl: 'ui/inline-edit/inline-edit-tpl.html',
+            link: linkFn
+        };
+
+
+
+        function linkFn(scope, element, attrs) {
+            var previousValue;
+
+            scope.edit = function () {
+                previousValue = scope.model;
+                scope.editModeActive = true;
+            };
+
+            scope.save = function () {
+                if (!scope.model && scope.model === '') { scope.cancel(); }
+                previousValue = scope.model;
+                scope.doSave({updates: scope.model});
+                scope.editModeActive = false;
+            };
+
+            scope.cancel = function () {
+                scope.model = previousValue;
+                scope.editModeActive = false;
+                scope.doCancel();
+            };
+            // scope.$watch('editModeActive', function (isEditMode) {
+            //     if (isEditMode) {
+            //         scope.$on('keyup:esc', function () {
+            //             scope.$apply(function () {
+            //                 scope.cancel();
+            //             });
+            //         });
+            //
+            //         scope.$on('keyup:enter', function () {
+            //             scope.$apply(function () {
+            //                 scope.save();
+            //             });
+            //         });
+            //     }
+            // });
+
+        }
+
+    }
+
+
+})(this);
