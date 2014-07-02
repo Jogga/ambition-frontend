3999bda791cc1a1e447bd466a8fc10164dae5b5d
Sebastian Fröstl <sebastian@froestl.com>
2014-07-02 09:18:29 +0200

    feature(functional) add module for functional programming

---
+++ src/app/common/functional.js
@@ -0,0 +1,25 @@
+(function(sandbox) {
+    'use strict';
+
+    sandbox.angular.module('amb.functional', [])
+    .factory('$f', f);
+
+    function f() {
+        return {
+            memoize: memoize
+        };
+
+        function memoize(fn) {
+            console.log(arguments);
+            var cache = {};
+            return function () {
+                var key = arguments[0];
+                if (!cache[key]) {                    
+                    cache[key] = fn.apply(this, arguments);
+                }
+                return cache[key];
+            };
+        }
+    }
+
+}(this));
