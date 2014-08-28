f5eac7a5b159eff63ce6831fb840ae0e389aa25f
Sebastian Fröstl <sebastian@froestl.com>
2014-08-28 09:32:40 +0200

    fix(new-ambition): resolve sharedData

--- src/app/screens/new-ambition/module.js
+++ src/app/screens/new-ambition/module.js
@@ -1,11 +1,13 @@
 sandbox.angular.module('amb.newAmbition', [
     'ngRoute',
+    'amb.sharedData',
     'amb.model.Ambition',
     'amb.newAmbition.newAmbitionController'
 ])
-.config(function ($routeProvider) {
+.config(function ($routeProvider, sharedDataProvider) {
     $routeProvider.when('/new-ambition', {
         templateUrl: 'screens/new-ambition/new-ambition-tpl.html',
-        controller: 'NewAmbitionController'
+        controller: 'NewAmbitionController',
+        resolve: sharedDataProvider.init()
     });
 });
